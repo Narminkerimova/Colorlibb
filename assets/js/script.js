fdd899a9fc4ff0c38b3e9d8f1263acd4760c96be
Narminkerimova <narmingk-bf206@code.edu.az>
2025-04-27 12:54:15 +0400

    second check

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const rezume_nav=document.querySelector(".rezume_nav")
 const remove_button=document.querySelector(".remove_button")
 const colorlib_nav=document.querySelector(".colorlib_nav")
 const main_nav=document.querySelector(".main_nav")
+const menu_nav=document.querySelector(".menu_nav")
+const menu_btn=document.getElementById("menu_btn")
 
 colorlib_rezume.addEventListener("click",function(){
     rezume_nav.classList.toggle("rezume_hidden")
@@ -10,4 +12,8 @@ colorlib_rezume.addEventListener("click",function(){
 remove_button.addEventListener("click",function(){
     colorlib_nav.classList.add("colorlib_hidden")
     main_nav.style.marginTop=("0vh")
+    menu_nav.style.marginTop=("10vh")
+})
+menu_btn.addEventListener("click",function(){
+    menu_nav.classList.toggle("hidden_menu")
 })
